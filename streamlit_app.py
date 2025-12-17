e581fd3f90f98afd4f35194d39322c695d060cee
Youngbin Kim <64558592+youngbinkim0@users.noreply.github.com>
2025-12-17 16:36:57 -0500

    Fix search value retrieval for selected fruit

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,8 +39,8 @@ if ingredients_list:
     ingredients_string = ''
     for fruit in ingredients_list:
         ingredients_string += fruit + ' '
-        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
-        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit,' is ', search_on, '.')
 
         st.subheader(fruit + ' Nutrition Information')
               
